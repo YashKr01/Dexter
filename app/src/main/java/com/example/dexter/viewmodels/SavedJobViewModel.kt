216a2e7bc4cf4bed277ef726fe8e939a979de4e3
yash <yashkumar201301@gmail.com>
2021-08-13 11:49:36 +0530

    Saved Jobs view model setup

--- app/src/main/java/com/example/dexter/viewmodels/SavedJobViewModel.kt
+++ app/src/main/java/com/example/dexter/viewmodels/SavedJobViewModel.kt
@@ -1,7 +1,22 @@
 package com.example.dexter.viewmodels
 
-class SavedJobViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dexter.model.JobEntity
+import com.example.dexter.repository.DatabaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
+class SavedJobViewModel @Inject constructor(private val repository: DatabaseRepository) :
+    ViewModel() {
 
+    fun insertJob(jobEntity: JobEntity) = viewModelScope.launch { repository.insertJob(jobEntity) }
+
+    fun deleteJob(jobEntity: JobEntity) = viewModelScope.launch { repository.deleteSavedJob(jobEntity) }
+
+    fun getSavedJobs(): LiveData<List<JobEntity>> = repository.getSavedJobs()
 
 }
